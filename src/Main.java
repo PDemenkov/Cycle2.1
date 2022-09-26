f0f07d542589e25a9691d94e6d47b9b19ee1926a
PDemenkov <guapnik@gmail.com>
2022-09-26 11:45:35 +0300

    Домашнее задание 3fix

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        int everyYear=79;
-        int beforeYear=1822;
-        int afterYear=2122;
-        for (int i = beforeYear;i<=afterYear;i++){
-            if (i%everyYear==0){
+        int everyYear = 79;
+        int beforeYear = 1822;
+        int afterYear = 2122;
+        for (int i = beforeYear; i <= afterYear; i++) {
+            if (i % everyYear == 0) {
                 System.out.println(i);
             }
         }
         System.out.println("Задание 2");
         int num = 2;
-        for (int i = 1; i<=10;i++){
-            System.out.println(num + "*" + i + " = " + num*i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + "*" + i + " = " + num * i);
         }
     }
 }
